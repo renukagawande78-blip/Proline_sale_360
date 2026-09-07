ac7325b3430667b808fdaeb5d943754cf3f191dc
renuka <renuka.gawande78@gmail.com>
2026-09-07 10:40:13 +0530

    chore(release): bump version to v2.5.0 with updated build timestamp

--- apps/web/src/config/version.ts
+++ apps/web/src/config/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = 'v2.4.0';
-export const APP_BUILD_DATE = '01-Sep-2026';
-export const APP_BUILD_TIME = '07:55 AM IST';
-export const APP_BUILD_DATETIME = '01-Sep-2026, 07:55 AM';
+export const APP_VERSION = 'v2.5.0';
+export const APP_BUILD_DATE = '07-Sep-2026';
+export const APP_BUILD_TIME = '10:40 AM IST';
+export const APP_BUILD_DATETIME = '07-Sep-2026, 10:40 AM';
